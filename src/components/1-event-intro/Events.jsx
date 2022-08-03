743ad26d8b90fe11f660ebb0f0b365e755887d77
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-03 16:33:37 +0300

    event

--- src/components/1-event-intro/Events.jsx
+++ src/components/1-event-intro/Events.jsx
@@ -1,9 +1,24 @@
 
-
+let info = "EVENTS";
 const Events = () => {
-  return (
-    <div>
-        <button className="btn btn-warning">Click</button>
+    const handleClick = () => {
+        alert("Btn Clicked")
+    }
+
+    const handleClear = (text) => {
+        alert(text) ;
+    }
+    const handleChange = (e) => {
+        info = "HOOKS";
+        console.log(info)
+        console.log(e.target)
+    }
+   return (
+    <div className="container text-center mt-4">
+        <div className="display-4 text-danger m-4">{info}</div>
+        <button onClick={handleClick} className="btn btn-warning">Click</button>
+        <button onClick={() => handleClear("Clear Btn pressed")} className="btn btn-dark">Clear</button>
+        <button onClick={(e) => handleChange(e)} className="btn btn-danger" >Change</button>
     </div>
   )
 }
