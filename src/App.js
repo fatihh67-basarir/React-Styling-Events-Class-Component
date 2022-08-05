cbf5048c9b6b7a5e571d36140dbdf521c315cb13
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-05 15:16:19 +0300

    hook start

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Events from "./components/1-event-intro/Events";
+import Counter from "./components/2-classComponents/Counter";
 
 
 
@@ -7,6 +8,8 @@ function App() {
   return (
     <div className="App">
      <Events/>
+     <Counter count = {10}/>
+     <Counter/>
         
     </div>
   );
---
+++ src/components/3-useStateHook/UseStateExample.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const UseStateExample = () => {
+  return (
+    <div>UseStateExample</div>
+  )
+}
+
+export default UseStateExample
